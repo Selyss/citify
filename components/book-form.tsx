14b8296384db274f86e35d77e39f52631cd5bde8
Selyss <99344963+Selyss@users.noreply.github.com>
2024-05-10 14:42:06 -0400

    redo form

--- components/book-form.tsx
+++
@@ -1,9 +0,0 @@
-import { Search } from "./search";
-
-export function BookForm() {
-  return (
-    <div className="space-y-1">
-      <Search />
-    </div>
-  );
-}
--- components/citation-form.tsx
+++ components/citation-form.tsx
@@ -1,35 +1,52 @@
-"use-client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-import { BookForm } from "@/components/book-form";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardFooter } from "@/components/ui/card";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+const citationStyles = z.enum(["mla9", "mla7", "chicago", "apa7"]);
+
+const formSchema = z.object({
+  format: z.enum(["website", "book"]),
+  style: citationStyles,
+  query: z.string(),
+});
 
 export function CitationForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      format: "website",
+      style: "mla9",
+      query: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   return (
-    <Tabs defaultValue="website" className="w-[400px]">
-      <TabsList className="grid w-full grid-cols-2">
-        <TabsTrigger value="website">Website</TabsTrigger>
-        <TabsTrigger value="book">Book</TabsTrigger>
-      </TabsList>
-      <TabsContent value="website">
-        <Card>
-          <CardContent className="space-y-2"></CardContent>
-          <CardFooter>
-            <Button>Generate Citation</Button>
-          </CardFooter>
-        </Card>
-      </TabsContent>
-      <TabsContent value="book">
-        <Card>
-          <CardContent className="space-y-2">
-            <BookForm />
-          </CardContent>
-          <CardFooter>
-            <Button>Generate Citation</Button>
-          </CardFooter>
-        </Card>
-      </TabsContent>
-    </Tabs>
+    <></>
+    //   <Tabs defaultValue="website" className="w-[400px]">
+    //     <TabsList className="grid w-full grid-cols-2">
+    //       <TabsTrigger value="website">Website</TabsTrigger>
+    //       <TabsTrigger value="book">Book</TabsTrigger>
+    //     </TabsList>
+    //     <TabsContent value="website">
+    //       <Card>
+    //         <CardContent className="space-y-2"></CardContent>
+    //         <CardFooter>
+    //           <Button>Generate Citation</Button>
+    //         </CardFooter>
+    //       </Card>
+    //     </TabsContent>
+    //     <TabsContent value="book">
+    //       <Card>
+    //         <CardContent className="space-y-2"></CardContent>
+    //         <CardFooter>
+    //           <Button>Generate Citation</Button>
+    //         </CardFooter>
+    //       </Card>
+    //     </TabsContent>
+    //   </Tabs>
   );
 }
