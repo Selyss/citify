985bc49d9a101290a8ea45e19e7bd438fdb7175a
Selyss <99344963+Selyss@users.noreply.github.com>
2024-05-12 16:46:52 -0400

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import { CitationForm } from "@/components/citation-form";
-import { TabRadio } from "@/components/tab-radio";
 
 export default function Home() {
   return (
     <div className="flex justify-center">
       <div className="flex flex-col items-center">
-        <TabRadio />
         <CitationForm />
       </div>
     </div>
