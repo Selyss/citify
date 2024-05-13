2a827d84d00cc35720063d5ac9d221e4725d7c24
Selyss <99344963+Selyss@users.noreply.github.com>
2024-05-13 12:47:43 -0400

    api working

--- components/citation-form.tsx
+++ components/citation-form.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 import {
   Form,
   FormControl,
@@ -34,6 +35,8 @@ const websiteSchema = z.object({
   query: z.string().url(),
 });
 
+const API = "https://api.bibify.org/api";
+
 export function CitationForm() {
   const web_form = useForm<z.infer<typeof websiteSchema>>({
     resolver: zodResolver(websiteSchema),
@@ -44,7 +47,10 @@ export function CitationForm() {
   });
 
   function onSubmit(data: z.infer<typeof websiteSchema>) {
-    console.log(data);
+    fetch(API + `/website?url=${data.query}`)
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
   }
   return (
     <div className="mx-auto max-w-md space-y-6">
@@ -62,63 +68,62 @@ export function CitationForm() {
         </TabsList>
         <TabsContent value="book"></TabsContent>
         <TabsContent value="website">
-          <Form {...web_form}>
-            <form
-              onSubmit={web_form.handleSubmit(onSubmit)}
-              className="space-y-8"
-            >
-              <FormField
-                control={web_form.control}
-                name="style"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Style</FormLabel>
-                    <Select
-                      onValueChange={field.onChange}
-                      defaultValue={field.value}
-                    >
-                      <FormControl>
-                        <SelectTrigger className="w-full">
-                          <SelectValue placeholder="Select citation style" />
-                        </SelectTrigger>
-                      </FormControl>
-                      <SelectContent>
-                        <SelectItem value="mla9">MLA 9</SelectItem>
-                        <SelectItem value="mla7">MLA 7</SelectItem>
-                        <SelectItem value="chicago">Chicago</SelectItem>
-                        <SelectItem value="apa7">APA 7</SelectItem>
-                      </SelectContent>
-                    </Select>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={web_form.control}
-                name="query"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Query</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Enter a website" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button type="submit">Generate Citation</Button>
-            </form>
-          </Form>
+          <Card className="pt-4">
+            <CardContent>
+              <Form {...web_form}>
+                <form
+                  onSubmit={web_form.handleSubmit(onSubmit)}
+                  className="space-y-4"
+                >
+                  <FormField
+                    control={web_form.control}
+                    name="style"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Style</FormLabel>
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                        >
+                          <FormControl>
+                            <SelectTrigger className="w-full">
+                              <SelectValue placeholder="Select citation style" />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            <SelectItem value="mla9">MLA 9</SelectItem>
+                            <SelectItem value="mla7">MLA 7</SelectItem>
+                            <SelectItem value="chicago">Chicago</SelectItem>
+                            <SelectItem value="apa7">APA 7</SelectItem>
+                          </SelectContent>
+                        </Select>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={web_form.control}
+                    name="query"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Query</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Enter a website" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <Button type="submit">Generate Citation</Button>
+                </form>
+              </Form>
+            </CardContent>
+          </Card>
         </TabsContent>
       </Tabs>
       <div className="space-y-2 text-center">
         <h2 className="text-2xl font-bold">Citation Preview</h2>
-        <div className="rounded-lg border bg-gray-50 p-4 text-left dark:bg-gray-900 dark:border-gray-800">
-          <p>
-            Author, A. A. (Year).
-            <em>Title of work</em>. Publisher.{"\n              "}
-          </p>
-        </div>
+        <div className="rounded-lg border bg-gray-50 p-4 text-left dark:bg-gray-900 dark:border-gray-800"></div>
       </div>
     </div>
   );
