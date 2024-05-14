21b0513e7fe0d55db25b80461b53434be88af06d
Selyss <99344963+Selyss@users.noreply.github.com>
2024-05-14 19:10:31 -0400

    remove book functionality for now

--- components/citation-form.tsx
+++ components/citation-form.tsx
@@ -21,7 +21,6 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useToast } from "@/components/ui/use-toast";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Key, useState } from "react";
@@ -125,94 +124,85 @@ export function CitationForm() {
         <h1 className="text-3xl font-bold">Citation Generator</h1>
         <p className="text-gray-500 dark:text-gray-400">
           Generate properly formatted citations in APA, MLA, or Chicago style
-          for books or websites.
+          for websites.
         </p>
       </div>
-      <Tabs className="w-full" defaultValue="website">
-        <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="book">Book</TabsTrigger>
-          <TabsTrigger value="website">Website</TabsTrigger>
-        </TabsList>
-        <TabsContent value="book"></TabsContent>
-        <TabsContent value="website">
-          <Card className="pt-4">
-            <CardContent>
-              <Form {...web_form}>
-                <form
-                  onSubmit={web_form.handleSubmit(onSubmit)}
-                  className="space-y-4"
-                >
-                  <FormField
-                    control={web_form.control}
-                    name="style"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Style</FormLabel>
-                        <Select
-                          onValueChange={field.onChange}
-                          defaultValue={field.value}
-                        >
-                          <FormControl>
-                            <SelectTrigger className="w-full">
-                              <SelectValue placeholder="Select citation style" />
-                            </SelectTrigger>
-                          </FormControl>
-                          <SelectContent>
-                            <SelectItem value="mla9">MLA 9</SelectItem>
-                            <SelectItem value="mla7">MLA 7</SelectItem>
-                            <SelectItem value="chicago">Chicago</SelectItem>
-                            <SelectItem value="apa7">APA 7</SelectItem>
-                          </SelectContent>
-                        </Select>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={web_form.control}
-                    name="query"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Query</FormLabel>
-                        <FormControl>
-                          <div className="grid w-full">
-                            <div className="grid w-full gap-1.5">
-                              <Textarea
-                                placeholder="Enter a website"
-                                {...field}
-                                id="message"
-                                rows={4}
-                              />
-                            </div>
-                          </div>
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <Button type="submit">Generate Citation</Button>
-                </form>
-              </Form>
-            </CardContent>
-          </Card>
-          <div className="space-y-2 pt-6 text-center">
-            <h2 className="text-2xl font-bold">Citation Preview</h2>
-            <div className="space-y-4">
-              {generatedCitations &&
-                generatedCitations.map(
-                  (citation: string, index: Key | null | undefined) => (
-                    <div
-                      className="rounded-lg border bg-gray-50 p-4 text-left dark:bg-gray-900 dark:border-gray-800"
-                      key={index}
-                      dangerouslySetInnerHTML={{ __html: citation }}
-                    />
-                  )
+      <Card className="pt-4">
+        <CardContent>
+          <Form {...web_form}>
+            <form
+              onSubmit={web_form.handleSubmit(onSubmit)}
+              className="space-y-4"
+            >
+              <FormField
+                control={web_form.control}
+                name="style"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Style</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger className="w-full">
+                          <SelectValue placeholder="Select citation style" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value="mla9">MLA 9</SelectItem>
+                        <SelectItem value="mla7">MLA 7</SelectItem>
+                        <SelectItem value="chicago">Chicago</SelectItem>
+                        <SelectItem value="apa7">APA 7</SelectItem>
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
                 )}
-            </div>
-            <Button onClick={copyToClipboard}>Copy to Clipboard</Button>
-          </div>
-        </TabsContent>
-      </Tabs>
+              />
+              <FormField
+                control={web_form.control}
+                name="query"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Query</FormLabel>
+                    <FormControl>
+                      <div className="grid w-full">
+                        <div className="grid w-full gap-1.5">
+                          <Textarea
+                            placeholder="Enter a website"
+                            {...field}
+                            id="message"
+                            rows={4}
+                          />
+                        </div>
+                      </div>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Generate Citation</Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+      <div className="space-y-2 pt-6 text-center">
+        <h2 className="text-2xl font-bold">Citation Preview</h2>
+        <div className="space-y-4">
+          {generatedCitations &&
+            generatedCitations.map(
+              (citation: string, index: Key | null | undefined) => (
+                <div
+                  className="rounded-lg border bg-gray-50 p-4 text-left dark:bg-gray-900 dark:border-gray-800"
+                  key={index}
+                  dangerouslySetInnerHTML={{ __html: citation }}
+                />
+              )
+            )}
+        </div>
+        <Button onClick={copyToClipboard}>Copy to Clipboard</Button>
+      </div>
     </div>
   );
 }
